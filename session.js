76cc3f3d4c535e881a00f3ba73db083c6c788177
WeruLK <mokokaranne@hotmail.com>
2025-04-10 17:46:15 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,9 +40,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~3Qs3maTY#TuQGrTWmhhnZOSCw0fnktq1EHB532eXM0WZUgFMv0ps",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "WeruLK",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "./session",
